1709d57fdc9b0cd455673d0f2a728f930796a9d7
David M <maniac@case-of.org>
2017-02-13 09:19:18 +0100

    Added sample code on how to use properties changed callback

--- src/main/java/com/github/hypfvieh/BlueMain.java
+++ src/main/java/com/github/hypfvieh/BlueMain.java
@@ -1,5 +1,8 @@
 package com.github.hypfvieh;
 
+import org.freedesktop.dbus.AbstractPropertiesHandler;
+import org.freedesktop.dbus.SignalAwareProperties.PropertiesChanged;
+
 import com.github.hypfvieh.control.EmbeddedShell;
 
 public class BlueMain {
@@ -9,6 +12,21 @@ public class BlueMain {
 
     public static void main(String[] _args) {
         boolean stopLoop = false;
+
+        // Sample on how to use PropertiesChanged callback
+        AbstractPropertiesHandler propertiesHandler = new AbstractPropertiesHandler() {
+            @Override
+            public void handle(PropertiesChanged _s) {
+                if (_s.getPropertiesChanged() != null && !_s.getPropertiesChanged().isEmpty()) {
+                    System.out.println("Got changed properties: " + _s.getPropertiesChanged());
+                }
+                if (_s.getPropertiesRemoved() != null && !_s.getPropertiesRemoved().isEmpty()) {
+                    System.out.println("Properties removed: " + _s.getPropertiesRemoved());
+                }
+            }
+        };
+
+        PaulmannDeviceController.getInstance().registerPropertyHandler(propertiesHandler);
 
         while (!stopLoop) {
             try(EmbeddedShell shell = new EmbeddedShell(System.in, System.out, System.err))  {
